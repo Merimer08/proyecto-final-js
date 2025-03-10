22d33b647f1aa0b108480f4ea9389b4e601c4b54
Merimer08 <marialolu08@gmail.com>
2025-03-10 11:38:28 +0100

    api y libro

--- src/main.js
+++ src/main.js
@@ -8,9 +8,35 @@ const elements = {
 };
 
 //Configuración: parametros de API
+const config = {
+    baseUrl: 'https://openlibrary.org/search.json', // URL base de la API para buscar libros.
+    coverUrl: 'https://covers.openlibrary.org/b/id', // URL base para obtener las imágenes de portada de los libros.
+    limit: 12 // Límite de resultados por búsqueda.
+};
 
-// Funciones auxiliares: Funciones que manipulan la interfaz de usuario.
+//Funciones auxiliares
+const showError = message => {
+    elements.error.style.display = 'block'; // Muestra el mensaje de error.
+    elements.error.textContent = message; // Establece el contenido del mensaje de error.
+};
 
 // Función para crear la tarjeta de cada libro que se mostrará en la página.
+const createBookCard = book => {
+        const {
+            title, // Título del libro.
+            cover_i: coverId, // ID de la portada del libro.
+            author_name: authors = ['Desconocido'], // Nombre del autor, si no se encuentra, se usa 'Desconocido'.
+            first_publish_year: year = 'Desconocido' // Año de la primera publicación del libro.
+        } = book;
 
-//Escuchamos el evento de envío del formulario para realizar la búsqueda.
+        // Genera la URL de la portada si existe el ID, sino asigna null.
+        const imageUrl = coverId ? `${config.coverUrl}/${coverId}-M.jpg` : null;
+
+        // Si hay imagen, se muestra, sino muestra un mensaje que dice que no hay portada disponible.
+        const imageElement = imageUrl ?
+            `<img src="${imageUrl}" alt="${title}" class="book-image">` :
+            `<div class="book-image no-image">Sin portada disponible</div>`;
+
+        // Retorna el HTML de la tarjeta con los datos del libro.
+
+        //Escuchamos el evento de envío del formulario para realizar la búsqueda.
