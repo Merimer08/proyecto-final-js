0bc7a3d8a91e723512de8d26c357c0fe7aa53cd3
Merimer08 <marialolu08@gmail.com>
2025-03-11 09:04:05 +0100

    Actualizada configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/proyecto-final-js/",
+    build: {
+        outDir: 'dist',
+    }
 });
